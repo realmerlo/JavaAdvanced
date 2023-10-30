cb437f34d71f52076e5a4fb57a873c26f1f5af19
Riccardo Merli <riccardo.merli017@gmail.com>
2023-10-30 12:05:02 +0100

    modded

--- src/Exceptions2/Exceptions2.java
+++ src/Exceptions2/Exceptions2.java
@@ -6,14 +6,14 @@ public class Exceptions2 {
             System.out.println(controlChar('t'));
         }
         catch (Exception e){
-            System.out.println("Error: the char isn't a number " + e);
+            System.out.println(e);
         }
 
     }
     public static boolean controlChar (char c) throws Exception{
         if(Character.isDigit(c)){
             return true;
-        } else throw new Exception();
+        } else throw new Exception("Error: the char isn't a number ");
     }
 }
 //Scrivere una funzione che controlli se un carattere è un numero altrimenti lanciare una eccezione.
--- src/Exceptions3/Exceptions3.java
+++ src/Exceptions3/Exceptions3.java
@@ -3,16 +3,19 @@ package Exceptions3;
 public class Exceptions3 {
     public static void main(String[] args) {
         try {
-            System.out.println(divider(10));
+            System.out.println(divider(10, 0));
         }
         catch (Exception e){
-            System.out.println("Error: the number cant be divided for 0 " + e);
+            System.out.println(e);
         }
 
     }
-    public static int divider (int num) throws ArithmeticException {
-        int output = num / 0;
-        return output;
+    public static int divider (int num, int div){
+       if(div != 0){
+           return num / 0;
+       } else {
+           throw new ArithmeticException("Error: the number cant be divided for 0 ");
+       }
     }
 }
 //Scrivere una funzione che provi a dividere un numero per 0 e catturi l'eccezione leggendone il messaggio.
